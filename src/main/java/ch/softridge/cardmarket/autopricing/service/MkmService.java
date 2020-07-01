eab63d25a4333e1667a5d847aaca15f296a37b6b
KZellweger <kevin.zellweger@students.fhnw.ch>
2020-07-01 23:04:29 +0200

    Build Correct Query Strings MKM Interface is up and running

--- src/main/java/ch/softridge/cardmarket/autopricing/service/MkmService.java
+++ src/main/java/ch/softridge/cardmarket/autopricing/service/MkmService.java
@@ -8,23 +8,23 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.ClassPathResource;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.DefaultUriBuilderFactory;
+import org.springframework.web.util.UriBuilderFactory;
 
 import javax.crypto.Mac;
 import javax.crypto.spec.SecretKeySpec;
 import javax.xml.bind.DatatypeConverter;
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.net.URLEncoder;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * @author Kevin Zellweger
@@ -48,44 +48,41 @@ public class MkmService {
     private HttpHeaders headers = new HttpHeaders();
     private static Logger log = LoggerFactory.getLogger(MkmService.class);
 
-    protected MkmService(){};
+    protected MkmService() {
+    }
 
-    public <T> T mkmRequest(Class<T> type,String route) {
+    ;
+
+    public <T> T mkmRequest(Class<T> type, String route) {
         String url = _apiUrl + route;
         //request(mkmRequest);
-        headers.set("Authorization",encryptHttpHeader(url));
+        headers.set("Authorization", encryptHttpHeader(url, null));
         entity = new HttpEntity(headers);
-        ResponseEntity<T> res = restTemplate.exchange(url, HttpMethod.GET,entity,type);
-        log.info(res.getStatusCode().toString());
-        log.info(res.getBody().toString());
+        ResponseEntity<T> res = restTemplate.exchange(url, HttpMethod.GET, entity, type);
+        log.info("MKM Return Code:" + res.getStatusCode().toString());
+        log.info("MKM Body:" + res.getBody().toString());
         return res.getBody();
     }
 
-    public <T> T mkmRequest(Class<T> type,String route,Map<String,String> params) {
+    public <T> T mkmRequest(Class<T> type, String route, Map<String, String> params) {
         String url = _apiUrl + route;
-        //request(mkmRequest);
-        headers.set("Authorization",encryptHttpHeader(url));
+        headers.set("Authorization", encryptHttpHeader(url, params));
         entity = new HttpEntity(headers);
-        ResponseEntity<T> res = restTemplate.exchange(url, HttpMethod.GET,entity,type,params);
-        log.info(res.getStatusCode().toString());
-        log.info(res.getBody().toString());
+        log.info(entity.toString());
+        url += "?";
+        StringBuilder sb = new StringBuilder();
+        sb.append(url);
+        params.forEach((k, v) -> sb.append(k+"="+v+"&"));
+        sb.deleteCharAt(sb.length()-1);
+        ResponseEntity<T> res = restTemplate.exchange(sb.toString(), HttpMethod.GET, entity, type);
+        log.info("MKM Return Code:" + res.getStatusCode().toString());
+        log.info("MKM Body:" + res.getBody().toString());
         return res.getBody();
     }
 
-    public <T> T mkmRequest(Class<T> type,String route,String params) {
-        String url = _apiUrl + route + params;
-        //request(mkmRequest);
-        headers.set("Authorization",encryptHttpHeader(url));
-        entity = new HttpEntity(headers);
-        ResponseEntity<T> res = restTemplate.exchange(url, HttpMethod.GET,entity,type);
-        log.info(res.getStatusCode().toString());
-        log.info(res.getBody().toString());
-        return res.getBody();
-    }
-
-    private String encryptHttpHeader(String url) {
-        log.info("URL: ",url);
-        Map<String, String> oAuthPorps = new HashMap<>();
+    private String encryptHttpHeader(String url, Map<String, String> queryParams) {
+        log.info("URL: ", url);
+        List<String> params = new ArrayList<>();
         String realm = url;
         String oauth_version = "1.0";
         String oauth_consumer_key = _mkmAppToken;
@@ -98,15 +95,31 @@ public class MkmService {
 
             String encodedRequestURL = rawurlencode(url);
             String baseString = "GET&" + encodedRequestURL + "&";
+            //Mandatory OAuth Params
+            params.add("oauth_consumer_key=" + rawurlencode(oauth_consumer_key));
+            params.add("oauth_nonce=" + rawurlencode(oauth_nonce));
+            params.add("oauth_signature_method=" + rawurlencode(oauth_signature_method));
+            params.add("oauth_timestamp=" + rawurlencode(oauth_timestamp));
+            params.add("oauth_token=" + rawurlencode(oauth_token));
+            params.add("oauth_version=" + rawurlencode(oauth_version));
+            if (queryParams != null) {
+                queryParams.forEach((k, v) -> {
+                    try {
+                        params.add(k + "=" + rawurlencode(v));
+                    } catch (UnsupportedEncodingException e) {
+                        log.error(e.getMessage());
+                    }
+                });
+            }
 
-            String paramString = "oauth_consumer_key=" + rawurlencode(oauth_consumer_key) + "&" +
-                    "oauth_nonce=" + rawurlencode(oauth_nonce) + "&" +
-                    "oauth_signature_method=" + rawurlencode(oauth_signature_method) + "&" +
-                    "oauth_timestamp=" + rawurlencode(oauth_timestamp) + "&" +
-                    "oauth_token=" + rawurlencode(oauth_token) + "&" +
-                    "oauth_version=" + rawurlencode(oauth_version);
+            params.sort(String::compareTo);
+            StringBuilder sb = new StringBuilder();
+            params.forEach(s -> sb.append(s + "&"));
+            sb.deleteCharAt(sb.length() - 1);
+            String paramString = sb.toString();
 
             baseString = baseString + rawurlencode(paramString);
+            log.info("baseString:"+baseString);
 
             String signingKey = rawurlencode(_mkmAppSecret) +
                     "&" +
@@ -128,17 +141,9 @@ public class MkmService {
                             "oauth_token=\"" + oauth_token + "\", " +
                             "oauth_signature_method=\"" + oauth_signature_method + "\", " +
                             "oauth_signature=\"" + oauth_signature + "\"";
-            oAuthPorps.put("realm", realm);
-            oAuthPorps.put("oauth_version", oauth_version);
-            oAuthPorps.put("oauth_timestamp", oauth_timestamp);
-            oAuthPorps.put("oauth_nonce", oauth_nonce);
-            oAuthPorps.put("oauth_consumer_key", oauth_consumer_key);
-            oAuthPorps.put("oauth_token", oauth_token);
-            oAuthPorps.put("oauth_signature_method", oauth_signature_method);
-            oAuthPorps.put("oauth_signature", oauth_signature);
         } catch (Exception e) {
 
-            log.error("(!) Error while requesting "+url);
+            log.error("(!) Error while requesting " + url);
         }
         return authorizationProperty;
     }
--- src/test/java/ch/softridge/cardmarket/autopricing/service/MkmServiceTest.java
+++ src/test/java/ch/softridge/cardmarket/autopricing/service/MkmServiceTest.java
@@ -10,10 +10,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.support.AnnotationConfigContextLoader;
-import org.springframework.validation.beanvalidation.SpringValidatorAdapter;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -25,7 +21,7 @@ import java.util.Map;
 @SpringBootTest(classes = {MkmService.class, Config.class})
 @EnableConfigurationProperties
 public class MkmServiceTest {
-    private static Logger log = LoggerFactory.getLogger(MkmServiceTest.class);
+    private static final Logger log = LoggerFactory.getLogger(MkmServiceTest.class);
     @Autowired
     private MkmService service;
 
@@ -43,18 +39,14 @@ public class MkmServiceTest {
 
     @Test
     public void testQueryAsMap(){
+        //TODO: Find suitable assertions
         String res;
         Map<String,String> queryParams = new HashMap<>();
         queryParams.put("search","Springleaf");
         queryParams.put("idGame","1");
         queryParams.put("idLanguage","1");
         res = service.mkmRequest(String.class,"products/find",queryParams);
+
     }
 
-    @Test
-    public void testQueryAsString(){
-        String res;
-        String queryParams = "?search=Springleaf&idGame=1&idLanguage=1";
-        res = service.mkmRequest(String.class,"products/find",queryParams);
-    }
 }
